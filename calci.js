c22ea4a29ebdc077263830ef4b7071335b851d4c
Shaikh Minhaj <107562768+minhaj-313@users.noreply.github.com>
2022-09-22 18:06:55 +0530

    Update calci.js

--- calci.js
+++ calci.js
@@ -1,13 +1,13 @@
 let outputScreen = document.getElementById("output-screen");
 
     function display(num) 
-{
+    {
     
         outputScreen.value += num;
     }
     
     function Calculate() 
-{
+   {
         try
         {
             outputScreen.value = eval(outputScreen.value);
