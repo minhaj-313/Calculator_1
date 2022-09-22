42cb4b958f12546eedd474753f639ecb76f43548
Shaikh Minhaj <107562768+minhaj-313@users.noreply.github.com>
2022-09-22 18:06:32 +0530

    Update calci.js

--- calci.js
+++ calci.js
@@ -21,7 +21,8 @@ let outputScreen = document.getElementById("output-screen");
 {
         outputScreen.value=""
     }
-    function del(){
+    function del()
+{
         outputScreen.value= outputScreen.value.slice(0,-1)
     }
     
